4916a26c7343108eb694699030321a003ce1efcf
LMKH <131709088+LMKH@users.noreply.github.com>
2023-06-28 11:41:18 +0100

    Add files via upload

---
+++ task_1_menu_new.py
@@ -0,0 +1,15 @@
+#asks user enters their choice
+print("Please enter what you would like to order")
+
+#user inputs their 3 choices
+order_1 = input("Order 1: ")
+order_2 = input("Order 2: ")
+order_3 = input("Order 3: ")
+
+#confirms order
+print("You have ordered the following dishes:")
+
+#prints out users choices
+print(order_1)
+print(order_2)
+print(order_3)
